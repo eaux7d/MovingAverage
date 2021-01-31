1dec4e169a29d018b95e71b481934f994b94b0c1
pablo <tretyak@lsystems.ru>
2021-01-31 11:23:51 +0400

    created MA function

---
+++ MovingAverage/MovingAverage.cpp
@@ -0,0 +1,22 @@
+#include "MovingAverage.h"
+
+template <typename T>
+void calculateAverage(T * signal, T * output, int const & winLen, int const & size)
+{
+	T sum = 0.0;
+
+	for (int i = 0; i != winLen; ++i)
+	{
+		sum += signal[i];
+		output[i] = sum / i;
+	}
+
+
+	for (int i = winLen; i != size; ++i)
+	{
+		sum -= signal[i - winLen];
+		sum += signal[i];
+
+		output[i] = sum / winLen;
+	}
+}
--- MovingAverage/MovingAverage.h
+++ MovingAverage/MovingAverage.h
@@ -1,1 +1,4 @@
 #pragma once
+
+template <typename T>
+void calculateAverage(T * signal, T * output, short const & window, int const & size);
