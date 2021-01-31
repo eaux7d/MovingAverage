2593e1ac652303747f95b04ded4b24eefd9111a9
pablo <tretyak@lsystems.ru>
2021-01-31 11:39:19 +0400

    added cmd line arguments for random seed and number of elements

--- MovingAverage/main.cpp
+++ MovingAverage/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include "MovingAverage.h"
 
-int main()
+int main(int argc, char * argv[])
 {
+	constexpr short wins[6] = { 4,8,16,32,64,128 };
+
+	int N = 0;
+	int randomSeed = 0;
+
+	if (argc > 2)
+	{
+		for (int i = 1; i != argc - 1; ++i)
+		{
+			if (argv[i][1] == 'N')
+				N = atoi(argv[i + 1]);
+
+			if (argv[i][1] == 'R')
+				randomSeed = atoi(argv[i + 1]);
+		}
+	}
+
+	std::cout << N << "\n" << randomSeed;
 
 	return 0;
 }
